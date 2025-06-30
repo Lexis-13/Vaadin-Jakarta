e9de1092f0486ada33cebdae13a1b372f8f9746a
Philip <p.splinter@ostfalia.de>
2025-06-30 18:32:14 +0200

    -BasicLayout konfiguriert - ToDoListView.java erweitert um den Titel

--- src/main/java/application/views/BasicLayout.java
+++ src/main/java/application/views/BasicLayout.java
@@ -1,15 +1,18 @@
 package application.views;
 
+import application.views.ToDoList.ToDoListView;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.html.H1;
 
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.RouterLink;
-
 
 /**
  *
@@ -38,7 +41,20 @@ public class BasicLayout extends AppLayout {
 
     public Component createNav(){
         VerticalLayout verticalLayout = new VerticalLayout();
-        verticalLayout.add(new RouterLink());
+        Icon icon = VaadinIcon.CLIPBOARD.create(); // oder ein anderes Icon
+        Span text = new Span("ToDo-List");
+
+        HorizontalLayout linkLayout = new HorizontalLayout(icon, text);
+        linkLayout.setSpacing(true);
+        linkLayout.setAlignItems(FlexComponent.Alignment.CENTER);
+
+        RouterLink link = new RouterLink();
+        link.add(linkLayout);
+        link.setRoute(ToDoListView.class);
+        link.setId("navbar-link");
+
+        verticalLayout.add(link);
+
         return verticalLayout;
     }
     public H1 getTitle(){
--- src/main/java/application/views/ToDoList/ToDoListView.java
+++ src/main/java/application/views/ToDoList/ToDoListView.java
@@ -1,6 +1,12 @@
 package application.views.ToDoList;
 
 import application.views.BasicLayout;
+import com.vaadin.flow.router.Route;
 
+@Route("/ToDo-List")
 public class ToDoListView extends BasicLayout {
+
+    public ToDoListView(){
+        this.getTitle().setText("ToDo-List");
+    }
 }
